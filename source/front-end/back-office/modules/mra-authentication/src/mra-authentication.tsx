63de0d3c889958731ed70a13cf4698c300d60350
Hoai Nguyen <hoai.nt235@gmail.com>
2021-06-01 21:55:11 +0700

    implement learn of fe and add styling material ui

--- source/front-end/back-office/modules/mra-authentication/src/mra-authentication.tsx
+++ source/front-end/back-office/modules/mra-authentication/src/mra-authentication.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import singleSpaReact from "single-spa-react";
-import Root from "./root.component";
+import MainLayout from "./layouts/MainLayout";
 
 const lifecycles = singleSpaReact({
   React,
   ReactDOM,
-  rootComponent: Root,
+  rootComponent: (props) => {
+    return <MainLayout>This is a test</MainLayout>;
+  },
   errorBoundary(err, info, props) {
     // Customize the root error boundary for your microfrontend here.
     return null;
