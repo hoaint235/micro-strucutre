264099753c968d08d4999670bb6b87bccfe58464
Hoai Nguyen <hoai.nt235@gmail.com>
2021-06-08 15:07:44 +0700

    init project

---
+++ source/back-end/Core/MicroArchitecture.Core/Interfaces/IAppModule.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace MicroArchitecture.Core.Interfaces
+{
+    public interface IAppModule
+    {
+        void RegisterServices(IServiceCollection service, IConfiguration configuration, Assembly[] assemblies);
+    }
+}
